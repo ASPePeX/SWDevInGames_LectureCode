fc394b3c6df53fa3daccf74a5610c83bec70c51e
Alexander Scheurer <mail@aspepex.net>
2024-05-08 12:55:49 +0200

    L05_1

---
+++ L05/L05Todos/Todo1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Todo1
+{
+    /// <summary>
+    /// Schaut Euch die Referenzdokumentation zu System.Type an und beantwortet folgende Fragen:
+    /// - Wie bekommt man eine Liste der in einer Klasse definierten Methoden?
+    /// - Wie bekommt man eine Liste der in einer Klasse definierten Felder / Eigenschaften?
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main()
+        {
+            var weirdThing = new WeirdType();
+            //weirdThing.get_Id();
+
+            Type thisMyClass = typeof(WeirdType);
+            Type auchMyClass = weirdThing.GetType();
+
+            if (thisMyClass.Equals(auchMyClass))
+                Console.WriteLine("Same");
+        }
+    }
+
+    internal class WeirdType
+    {
+        public float Scale;
+
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; set; }
+
+        public WeirdType()
+        {
+            Name = "";
+            Description = "";
+        }
+
+        public WeirdType(string name = "", string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public void DoWeirdThing() { }
+        public void DoSomethingEvenWeirder(int id) { }
+    }
+}
---
+++ L05/L05Todos/Todo2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace Todo2
+{
+    /// <summary> 
+    /// - Ruft auf Objekten von primitiven Datentypen (int, string) die Methode GetType() auf und inspiziert den Inhalt der jeweils zurückgegebenen Typ-Instanz mit dem Debugger oder gebt Informationen, die von der Instanz abrufbar sind, per WriteLine aus.
+    /// - Probiert das Ganze mit selbst definierten Datentypen. Nicht nur mit class und struct, sondern auch mit enum und delegate.
+    /// - Schreibt eine Beispiel-Assembly (DLL oder EXE), in der ein oder mehrere Datentypen implementiert werden. Erzeugt ein weiteres (Konsolen-)Projekt, in dem Ihr die Beispiel-Assembly mit LoadFrom (s.o) ladet und die enthaltenen Typen (GetTypes) abfragt und von diesen ein paar Eigenschafte (z.B. Name) ausgebt. Tauscht die Beispiel-Assemblies ohne Source-Code untereinander aus.
+    /// </summary>
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Type intType = typeof(int);
+            Type stringType = typeof(string);
+
+            Type myClassType = typeof(MyClass);
+            Type mySubClassType = typeof(MySubClass);
+            Type myStructType = typeof(MyStruct);
+            Type myEnumType = typeof(MyEnum);
+            Type myDelType = typeof(MyDelegate);
+
+            var types = Assembly.LoadFrom("Todo2.Supplemental.dll");
+        }
+    }
+
+    internal class MyClass { }
+    internal class MySubClass : MyClass { }
+    internal struct MyStruct { }
+    internal enum MyEnum { one, two }
+    internal delegate void MyDelegate(int n);
+}
