978612568f47baedb45308d181216fd6f4bedef6
Alexander Scheurer <mail@aspepex.net>
2024-05-15 12:14:01 +0200

    L05_3

---
+++ L05/L05Todos/Todo4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Todo4
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var type = typeof(MyClass);
+        }
+    }
+
+    [ClassInfo]
+    public class MyClass
+    {
+        [MethodInfo]
+        public void DoSomething([ParamInfo] int a) { }
+
+        [FieldInfo]
+        public int Number;
+    }
+
+    public class XAttribute : Attribute { }
+    public class MethodInfoAttribute : Attribute { }
+    public class ParamInfoAttribute : Attribute { }
+    public class FieldInfoAttribute : Attribute { }
+}
